5a3cdfeaae557999243a72bdb48a85f96518b2a7
Di <dimas2343@mail.ru>
2022-08-20 22:36:17 +0300

    A^b возвести

---
+++ 001_A^b возвести/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numA = ReadInt("Введите число: ");
+int numB = ReadInt("Введите степень: ");
+
+int sum = 1;
+
+while(numB >=1)
+{
+    sum *= numA;
+    numB--;
+}
+
+Console.WriteLine(sum);
+
---
+++ 003_выводит массив из N элементов/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+// 5 -> [1, 2, 5, 7, 19]
+// 3 -> [6, 1, 33]
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [num];
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 101);
+    }
+}
+
+FillArray(array);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+PrintArray(array);
